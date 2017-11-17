653c9fa85fd08ef9bc9c9a82b3d3ba1af7f2c6fa
zizouvb <zizouvb@users.noreply.github.com>
2017-11-17 19:13:10 +0100

    [Update] to Wood2 rules + Wait for Wood1

--- Multiplayers/CGcompetions.py
+++ Multiplayers/CGcompetions.py
@@ -7,9 +7,11 @@ import math
 
 # game loop
 while True:
-    dist = 10000000000
-    goal_x=0
-    goal_y=0
+    distp = 10000000000
+    distd = 10000000000
+    gp_x= gd_x = gd_y = d_x = d_y = 0
+    gp_y=0
+    
     my_score = int(input())
     enemy_score_1 = int(input())
     enemy_score_2 = int(input())
@@ -32,16 +34,25 @@ while True:
         vy = int(vy)
         extra = int(extra)
         extra_2 = int(extra_2)
-        if unit_id==0:
+        if unit_id==0 and unit_type==0:
             p_x = x
             p_y = y
-        if unit_type==4 and (x-p_x)**2 + (y-p_y)**2<dist:
-            goal_x = x    
-            goal_y = y
-            dist = (goal_x-p_x)**2 + (goal_y-p_y)**2
+        if unit_id==0 and unit_type==1:
+            d_x = x
+            d_y = y
+        if unit_type==4 and (x-p_x)**2 + (y-p_y)**2<distp:
+            gp_x = x    
+            gp_y = y
+            distp = (gp_x-p_x)**2 + (gp_y-p_y)**2
+        if unit_type==3 and (gd_x-d_x)**2 + (gd_y-d_y)**2<distd:
+            gd_x = x    
+            gd_y = y
+            distd = (gd_x-d_x)**2 + (gd_y-d_y)**2
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr)
-
-    print(goal_x, goal_y, 300)
-    print("WAIT")
+    if distp>1000000000:
+        print(gd_x, gd_y, 300)
+    else:
+        print(gp_x, gp_y, 300)
+    print(gd_x, gd_y, 300)
     print("WAIT")
