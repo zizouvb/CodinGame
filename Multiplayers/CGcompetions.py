e578ad3b61cdd8de6509eeb19c31d96de0a40bc6
zizouvb <zizouvb@users.noreply.github.com>
2017-11-20 02:24:43 +0100

    [Update] refactoring

--- Multiplayers/CGcompetions.py
+++ Multiplayers/CGcompetions.py
@@ -4,13 +4,56 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
+class Unity():
+    def __init__(self,unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2):
+        self.unid_id = unit_id
+        self.unit_type = unit_type
+        self.player = player
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.mass = mass
+        self.radius = radius
+        self.extra = extra
+        self.extra_2 = extra
+
+    def squared_distance(self, unity):
+        if unity:
+            return (self.x-unity.x)**2+(self.y-unity.y)**2 
+        else:
+            return 100000000
+
+
+class Tanker(Unity):
+    def __init__(self,unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2):
+        Unity.__init__(self,unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2)
+    
+
+
+class Reaper(Unity):
+    def __init__(self,unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2):
+        Unity.__init__(self,unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2)
+        self.wreck = None 
+        print("reaper instanciation", file=sys.stderr)
+
+class Doof(Unity):
+    def __init__(self,unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2):
+        Unity.__init__(self,unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2)
+
+class Destroyer(Unity):
+    def __init__(self,unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2):
+        Unity.__init__(self,unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2)
+        self.tanker = None
+        print("destroyer instanciation", file=sys.stderr)
+
+class Wreck(Unity):
+    def __init__(self,unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2):
+        Unity.__init__(self,unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2)
+
 
 # game loop
 while True:
-    distp = 10000000000
-    distd = 10000000000
-    gp_x= gd_x = gd_y = d_x = d_y = 0
-    gp_y=0
     
     my_score = int(input())
     enemy_score_1 = int(input())
@@ -19,7 +62,8 @@ while True:
     enemy_rage_1 = int(input())
     enemy_rage_2 = int(input())
     unit_count = int(input())
-    
+    list_wrecks = []
+    list_tankers = []
     for i in range(unit_count):
         unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2 = input().split()
         
@@ -34,25 +78,37 @@ while True:
         vy = int(vy)
         extra = int(extra)
         extra_2 = int(extra_2)
-        if unit_id==0 and unit_type==0:
-            p_x = x
-            p_y = y
-        if unit_id==0 and unit_type==1:
-            d_x = x
-            d_y = y
-        if unit_type==4 and (x-p_x)**2 + (y-p_y)**2<distp:
-            gp_x = x    
-            gp_y = y
-            distp = (gp_x-p_x)**2 + (gp_y-p_y)**2
-        if unit_type==3 and (gd_x-d_x)**2 + (gd_y-d_y)**2<distd:
-            gd_x = x    
-            gd_y = y
-            distd = (gd_x-d_x)**2 + (gd_y-d_y)**2
+        if player==0 and unit_type==0:
+            reaper = Reaper(unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2)
+        if player==0 and unit_type==1:
+            destroyer = Destroyer(unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2)
+        if player==0 and unit_type==2:
+            doof = Doof(unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2)
+        if unit_type==4: 
+            list_wrecks.append(Wreck(unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2))
+            
+        if unit_type==3:
+            list_tankers.append(Tanker(unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2))
+            
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr)
-    if distp>1000000000:
-        print(gd_x, gd_y, 300)
+    for wreck in list_wrecks:
+        if reaper.squared_distance(wreck)<reaper.squared_distance(reaper.wreck):
+                reaper.wreck = wreck 
+
+    
+    for tanker in list_tankers:
+        if destroyer.squared_distance(tanker)<destroyer.squared_distance(destroyer.tanker):
+                destroyer.tanker = tanker
+
+
+
+    if not reaper.wreck:
+        print(destroyer.tanker.x,destroyer.tanker.y, 300)
     else:
-        print(gp_x, gp_y, 300)
-    print(gd_x, gd_y, 300)
+        print(reaper.wreck.x, reaper.wreck.y, 300)
+    if not destroyer.tanker:
+        print(0,0, 300)
+    else:
+        print(destroyer.tanker.x,destroyer.tanker.y, 300)
     print("WAIT")
